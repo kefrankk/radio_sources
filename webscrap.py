0341577ab0e6acfde8d178767840f414ca1a58ea
kefrankk <ke.heckler95@gmail.com>
2024-02-12 16:05:27 -0300

    find out how to clear the form and add galaxy position

--- webscrap.py
+++ webscrap.py
@@ -1,14 +1,19 @@
+import time
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
+
+# galaxies infos
 columns = ['NSAID', 'RAdeg-gal', 'DEdeg-gal', 'RAdeg-spec', 'DEdeg-spec', 'Sep', 'z', 'mass', 'SFR', 'metallicity', 'Ref', 'Class']
 
 rows = []
-galaxy = []
-with open('galaxy_properties.txt', 'r') as prop:
+gal = []
+with open('Molina_Dwarf_sample.txt', 'r') as prop:
 
     for line_number, line in enumerate(prop):
         if line_number > 43:
@@ -17,18 +22,71 @@ with open('galaxy_properties.txt', 'r') as prop:
 
     
     for i in range(len(rows)):
-        galaxy.append(rows[i])
+        gal.append(rows[i])
 
 
-properties = pd.DataFrame(galaxy, columns=columns)
+properties = pd.DataFrame(gal, columns=columns)
 
 RA_gal, dec_gal = properties['RAdeg-gal'].values, properties['DEdeg-gal'].values
 
+# tranforming to string to be able to use selenium for research
+galaxy = []
+for o in range(len(RA_gal)):
+    galaxy.append([str(RA_gal[o]) + ' ' +str(dec_gal[o])])
 
+# print(galaxy)
+
+
+url = 'https://sundog.stsci.edu/cgi-bin/searchfirst'
+
+# using selenium to serach for our query
+driver = webdriver.Chrome()
+driver.get(url)
+driver.implicitly_wait(10)
+# driver.maximize_window()
+
+# time.sleep(1)
+# print(driver.title)
+
+# frame = driver.find_elements_by_tag_name('input')
+
+
+driver.find_element(By.XPATH, '//input[@id="pos"]').clear()
+time.sleep(1)
+form_radec = driver.find_element(By.XPATH, '//input[@id="pos"]').send_keys(galaxy[0])
+
+time.sleep(1)
+
+
+
+
+
+# label = driver.find_element(By.CSS_SELECTOR, 'label[for="RA"]')
+
+driver.quit()
+
+
+# def search_galaxy(position):
+#     driver = webdriver.Chrome()
+    
+#     try: 
+
+#         url = 'https://sundog.stsci.edu/cgi-bin/searchfirst'
+#         driver.get(url)
+
+        
+
+
+    # except:
+    #     print('abri o site!')
+
+
+
+
+exit()
 
 # opening the website where we want to search for our query
 
-url = 'https://sundog.stsci.edu/cgi-bin/searchfirst'
 
 response = requests.get(url)
 
@@ -42,6 +100,8 @@ inputs = []
 for link in soup.find_all('input'):
     inputs.append(link.get('value'))
 print(inputs)
+
+
 
 
 
