7f8cd6bd634987c8e3bd77b86badeed562f84ae4
kefrankk <ke.heckler95@gmail.com>
2024-02-11 23:25:56 -0300

    listei links disponiveis em um site qualquer

--- webscrap.py
+++ webscrap.py
@@ -1,2 +1,20 @@
 
-# import requests
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.infomoney.com.br/'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+links = []
+for link in soup.find_all('a'):
+    links.append(link.get('href'))
+
+print(len(links))
+
+
+
+# print(soup.find_all('href'))
