63c2757c24a9f3a8c0697aad648313fe3deedcf3
kefrankk <ke.heckler95@gmail.com>
2024-02-12 14:39:14 -0300

    acessei o site de pesquisa do FIRST

--- webscrap.py
+++ webscrap.py
@@ -1,19 +1,49 @@
-
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
-url = 'https://www.infomoney.com.br/'
+columns = ['NSAID', 'RAdeg-gal', 'DEdeg-gal', 'RAdeg-spec', 'DEdeg-spec', 'Sep', 'z', 'mass', 'SFR', 'metallicity', 'Ref', 'Class']
+
+rows = []
+galaxy = []
+with open('galaxy_properties.txt', 'r') as prop:
+
+    for line_number, line in enumerate(prop):
+        if line_number > 43:
+            row = line.split()
+            rows.append(row)
+
+    
+    for i in range(len(rows)):
+        galaxy.append(rows[i])
+
+
+properties = pd.DataFrame(galaxy, columns=columns)
+
+RA_gal, dec_gal = properties['RAdeg-gal'].values, properties['DEdeg-gal'].values
+
+
+
+# opening the website where we want to search for our query
+
+url = 'https://sundog.stsci.edu/cgi-bin/searchfirst'
 
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+if response.status_code == 200:
 
-links = []
-for link in soup.find_all('a'):
-    links.append(link.get('href'))
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-print(len(links))
+
+# # print(soup)
+inputs = []
+for link in soup.find_all('input'):
+    inputs.append(link.get('value'))
+print(inputs)
+
+
 
 
 
